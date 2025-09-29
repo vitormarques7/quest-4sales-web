1177854911fd947a9b897f5930b696152fe13c03
vitormarques7 <joaovitormarquesbr@gmail.com>
2025-09-29 00:44:55 -0300

    Initial commit

---
+++ src/main/java/com/example/quest_4sales_web/Quest4salesWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.quest_4sales_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quest4salesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quest4salesWebApplication.class, args);
+	}
+
+}
