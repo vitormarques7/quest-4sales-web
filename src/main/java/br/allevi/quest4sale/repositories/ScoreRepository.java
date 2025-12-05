353624552ace0b18f3d13dbfbe25945e630bf306
Vitor <joaovitormarquesbr@gmail.com>
2025-12-05 01:25:01 -0300

    feat: adiciona método findBySaleId para evitar pontuação duplicada

--- src/main/java/br/allevi/quest4sale/repositories/ScoreRepository.java
+++ src/main/java/br/allevi/quest4sale/repositories/ScoreRepository.java
@@ -6,10 +6,13 @@ import br.allevi.quest4sale.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ScoreRepository extends JpaRepository<Score, UUID> {
     List<Score> findByUserAndCompetition(User user, Competition competition);
+
+    Optional<Score> findBySaleId(UUID saleId);
 }
 
 
